b0950a6b8e284382236e24e802da75b8815fcec2
fengxiaoyun <fengxiaoyun1026@126.com>
2022-06-05 19:14:39 +0800

    递归算法

--- demo1/demo.cpp
+++ demo1/demo.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
 using namespace std;
+
+//计算阶乘  递归用法
+int  fact (int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*fact(n-1);
+    }
+    
+}
+
 int main()
 {
-    cout<<"hello";
+  
+
+    int n = 3;
+    cout<<fact(n)<<endl;
+
+    cout<<"hello"<<endl;
     return 0;
 }
